5545330b0d6fdd9b562cf394c32f5fbe1ec8509d
gvenezia14 <gvenezia@wustl.edu>
2016-04-25 18:41:28 -0500

    ,m

--- js/script.js
+++ js/script.js
@@ -1,12 +1,25 @@
-// loop
-var count = 0;
-var list = '';
 
-while (count < 10) {
-  list += '<li>current count is: ' + count + '</li>';
-  count += 1;
+//
+function question(questionText){
+  order = prompt(questionText);
 }
 
-// output results
-document.write("<h1>Loops</h1>");
-document.write("<ul>" + list + "</ul>");
+//array
+var menuPrompts = [
+  ["What type of meat?"],
+  ["What type of bean?"],
+];
+
+//While loop
+var secondList = "<ul>";
+var i = 0;
+
+while (i < menuPrompts.length){
+  secondList += "<li>" + menuPrompts[i]+ " response: " + prompt(menuPrompts[i]) +"</li>";
+  i++;
+}
+
+secondList +="</ul>";
+
+//Print it out
+document.write(secondList);
